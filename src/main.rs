456ea44f0731e59dc65834df411b0204c6641aa7
Mario Cristovao <mjpc13@protonmail.com>
2024-04-18 11:27:57 +0100

    Add iG-LIO and BotanicGarden Dataset

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use bollard::{API_DEFAULT_VERSION, Docker};
-
+use plotters::prelude::*;
 
 use rustle::db::DB;
 use rustle::evo_wrapper;
 
 use rustle::evo_wrapper::EvoArgs;
 use rustle::metrics::Metrics;
-use rustle::metrics::Stats;
+use rustle::metrics::ContainerStats;
 use tokio;
 
 use rustle::task::Config;
@@ -29,37 +29,85 @@ async fn main() {
     init();
 
     let dataset_path = "/home/mario/Documents/rustle/test/dataset/";
-    let params_path = "/home/mario/Documents/rustle/test/config/";
+    let params_path = "/home/mario/Documents/rustle/test/config/iG-LIO/";
 
     let topics: Vec<String> = vec![
-       "/lio_sam/mapping/odometry".to_string(), 
-       "/lio_sam/mapping/odometry_incremental".to_string()
+       "/lio_odom".to_string(), 
     ];
+    //let topics_lio-sam: Vec<String> = vec![
+    //    "/lio_sam/mapping/odometry".to_string(), 
+    //    "/lio_sam/mapping/odometry_incremental".to_string()
+    //];
+    //let file = "file://test/db/";
 
-    //let test = Config::new(
-    //    "mjpc13/rustle:lio-sam".into(), 
-    //    dataset_path.into(), 
-    //    params_path.into(), 
-    //    topics, 
-    //    None, //optionally we can pass a custom Docker socket
-    //    None // connect to a custom (and persistent) Surreal database. TODO: Don't pass a database but the endpoint!
-    //).await;
+    let test = Config::new(
+        "mjpc13/rustle:ig-lio".into(), 
+        dataset_path.into(), 
+        params_path.into(), 
+        topics, 
+        None, //optionally we can pass a custom Docker socket
+        None
+        //Some(file) // connect to a custom (and persistent) Surreal database. TODO: Don't pass a database but the endpoint!
+    ).await;
     
-    //let task1: Task = Task::new(test.unwrap()).await;
-    //let _result = task1.run().await.unwrap();
+    let task1: Task = Task::new(test.unwrap()).await;
+    let _result = task1.run().await.unwrap();
 
-    //RUN EVO AND EXTRACT RESULTS
-    let groundtruth = "/home/mario/Documents/rustle/test/evo/groundtruth.txt";
-    let data = "/home/mario/Documents/rustle/test/evo/data.txt";
+
+
+
+
+//    //RUN EVO AND EXTRACT RESULTS
+//    let groundtruth = "/home/mario/Documents/rustle/test/evo/groundtruth.txt";
+//    let data = "/home/mario/Documents/rustle/test/evo/data.txt";
+//    
+//    //The test db;
+//    let file = "file://test/db/";
+//    let d = any::connect(file).await.unwrap();
+//    d.use_ns("namespace").use_db("database").await.unwrap();
+//
+//    let db = DB {db: d};
+//    let vec_stats: Vec<ContainerStats> = db.query_stats().await.unwrap(); 
+//    let evo_args = EvoArgs::default();
+//
+//
+//
+//    let root = BitMapBackend::new("/home/mario/Documents/rustle/0.png", (1920, 1080)).into_drawing_area();
+//    root.fill(&WHITE).unwrap();
+//
+//    let mut chart = ChartBuilder::on(&root)
+//        .caption("Memory Usage (MiB)", ("sans-serif", 50).into_font())
+//        .margin(5)
+//        .x_label_area_size(50)
+//        .y_label_area_size(50)
+//        .build_cartesian_2d(0u32..562u32, 100f64..1000f64).unwrap();
+//    
+//
+//    chart.configure_mesh().draw().unwrap();
+//
+//    chart
+//        .draw_series(LineSeries::new(
+//                vec_stats.into_iter().map(|s| 
+//                    (s.uid.unwrap(), s.memory_stats.usage.unwrap() as f64 * 1e-6)
+//                ),
+//            //(-50..=50).map(|x| x as f64 / 50.0).map(|x| (x, x * x)),
+//            &RED,
+//        )).unwrap()
+//        .label("LIO-SAM")
+//        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+//
+//    chart
+//        .configure_series_labels()
+//        .background_style(&WHITE.mix(0.8))
+//        .border_style(&BLACK)
+//        .draw().unwrap();
+//
+//    root.present().unwrap();
+//
     
-    //The test db;
-    let file = "file://db/";
-    let d = any::connect(file).await.unwrap();
-    d.use_ns("namespace").use_db("database").await.unwrap();
-    let db = DB {db: d};
 
-    let vec_stats: Vec<Stats> = db.query_stats().await.unwrap(); 
-    let evo_args = EvoArgs::default();
+
+
 
     //let m = evo_wrapper::evo_ape(groundtruth, data, EvoArgs::default());
     //let m = Metrics::compute(groundtruth, data, EvoArgs::default());
